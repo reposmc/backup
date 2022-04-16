67a683608aa5c787947f37dbf0e8fb9be4b195fb
Leonel López <92056647+leolopezMC@users.noreply.github.com>
2022-04-16 17:22:20 -0600

    Enviroment variables

--- src/config/backup.php
+++ src/config/backup.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'tenant_id' => '',
-    'client_id' => '',
-    'client_secret' => '',
-    'username' => '',
-    'password' => '',
+    'tenant_id' => env('TENANT_ID'),
+    'client_id' => env('CLIENT_ID'),
+    'client_secret' => env('CLIENT_SECRET'),
+    'username' => env('USERNAME'),
+    'password' => env('PASSWORD'),
 ];
